eb002bd1376612e078c48e2e488435ed25467bb3
Andy Roxby <Andy.Roxby@gmail.com>
2017-10-29 22:29:20 -0400

    Create dummy test

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
-#include <gtest/gtest.h>
 #include <system.h>
+#include <gtest/gtest.h>
 
-int main() {
-    std::cout << "Tests go here\n";
-    return 0;
+class SystemTest : public testing::Test {
+protected:
+    System sys;
+};
+
+// Tests the default c'tor.
+TEST_F(SystemTest, ExampleTest) {
+    EXPECT_EQ(0, 0);
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+  printf("Running main() from gtest_main.cc\n");
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
