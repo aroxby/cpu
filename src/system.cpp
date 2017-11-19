652b87a7f2df8c9b91041cce34e7c0290f9f72dd
Andy Roxby <Andy.Roxby@gmail.com>
2017-11-19 06:44:36 -0500

    Implement memory install/uninstall

--- src/system.cpp
+++ src/system.cpp
@@ -1,33 +1,81 @@
 #include "system.h"
 
-int System::installMemory(MemoryModule *mod, SizeType offset, SizeType size){
+// Find installed moudle with base address <= address (or NULL)
+const MemoryInstance *System::resolveAtMost(SizeType address) const {
+    auto iterator = mem.lower_bound(address);
+    if(iterator->second.offset != address) {
+        if (iterator == mem.cbegin()) {
+            iterator = mem.cend();
+        } else if(iterator != mem.cend()) {
+            iterator--;
+        }
+    }
+    return iterator->second;
+}
+
+// Find installed moudle with base address >= address (or NULL)
+const MemoryInstance *System::resolveAtLeast(SizeType address) const {
+    auto iterator = mem.lower_bound(address);
+    return iterator->second;
+}
+
+// Get the module to lowest address after inst (or NULL)
+const MemoryInstance *System::next(const MemoryInstance inst) const {
+    auto iterator = mem.find(inst.offset);
+    if(iterator != mem.cend()) {
+        iterator++;
+    }
+    return iterator->second;
+}
+
+int System::installMemory(MemoryModule &mod, SizeType offset, SizeType size) {
+    int status = ERR_SUCCESS;
+
+    if(size <= 0) {
+        status = ERR_BADRANGE;
+    } else {
+        const MemoryInstance *overlap = resolveAtMost(offset + size - 1);
+        if(overlap) {
+            SizeType overlapEnd = overlap->offset + overlap->size - 1;
+            if(overlapEnd >= offset) {
+                status = ERR_CONFLICT;
+            }
+        }
+    }
+
+    if(status == ERR_SUCCESS) {
+        mem[offset] = MemoryInstance(mod, offset, size);
+    }
+    return status;
+}
+
+int System::removeMemory(SizeType offset) {
+    MemMap::size_type erased = mem.erase(offset);
+    assert erased <= 1;
+    int status = erased ? ERR_SUCCESS : ERR_BADRANGE;
+    return status;
+}
+
+int System::bindPort(PortSocket *sock, PortType port) {
     throw "Not Implemented";
 }
 
-int System::removeMemory(SizeType offset){
+int System::releasePort(PortType port) {
     throw "Not Implemented";
 }
 
-int System::bindPort(PortSocket *sock, PortType port){
+int System::readMemory(SizeType offset, SizeType len, void *data) const {
     throw "Not Implemented";
 }
 
-int System::releasePort(PortType port){
+int System::writeMemory(SizeType offset, SizeType len, const void *data) const {
     throw "Not Implemented";
 }
 
-int System::readMemory(SizeType offset, SizeType len, void *data){
+int System::readPort(PortType port, SizeType len, void *data) const {
     throw "Not Implemented";
 }
 
-int System::writeMemory(SizeType offset, SizeType len, const void *data){
-    throw "Not Implemented";
-}
-
-int System::readPort(PortType port, SizeType len, void *data){
-    throw "Not Implemented";
-}
-
-int System::writePort(PortType port, SizeType len, const void *data){
+int System::writePort(PortType port, SizeType len, const void *data) const {
     throw "Not Implemented";
 }
--- src/system.h
+++ src/system.h
@@ -1,6 +1,7 @@
 #ifndef _INC_SYSTEM_H
 #define _INC_SYSTEM_H
 
+#include <map>
 #include "memorymodule.h"
 #include "portsocket.h"
 
@@ -9,13 +10,13 @@ class System {
 // All methods return 0 on success and non-zero on error
 public:
     // Installs mod to support the memory at offset for size
-    int installMemory(MemoryModule *mod, SizeType offset, SizeType size);
+    int installMemory(MemoryModule &mod, SizeType offset, SizeType size);
 
     // Removes the MemoryModule that supports the offset
     int removeMemory(SizeType offset);
 
     //Installs sock as the read/write interface on port
-    int bindPort(PortSocket *sock, PortType port);
+    int bindPort(PortSocket &sock, PortType port);
 
     // Removes the PortSocket on port
     int releasePort(PortType port);
@@ -31,6 +32,24 @@ public:
 
     // Write data to port
     int writePort(PortType port, SizeType len, const void *data);
+
+private:
+    class MemoryInstance {
+    public:
+        SizeType offset;
+        SizeType length;
+        MemoryModule &mod;
+
+        MemoryInstance(MemoryModule &mod, SizeType offset, SizeType length) :
+            mod(mod), offset(offset), length(length) {
+        }
+    };
+
+    typedef std::map<SizeType, const MemoryInstance> MemMap;
+    typedef std::map<PortType, PortSocket*> PortMap;
+
+    MemMap mem;
+    PortMap ports;
 };
 
 #endif//_INC_SYSTEM_H
