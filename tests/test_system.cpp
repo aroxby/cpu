6c473712469389a9e2fba6ef4cc5088d7d615c69
Andy Roxby <Andy.Roxby@gmail.com>
2017-11-20 02:49:02 -0500

    Create example memory read test

--- tests/test_system.cpp
+++ tests/test_system.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <errors.h>
 #include <system.h>
 #include "mock_memorymodule.h"
 
@@ -11,6 +12,11 @@ protected:
 //Dumy test
 TEST_F(SystemTest, TestMemoryAddressTranslation) {
     unsigned char dummy_buffer[10];
-    sys.installMemory(mem, 10, 10);
-    int iret = sys.readMemory(10, 5, dummy_buffer);
+    int iret;
+
+    iret = sys.installMemory(mem, 10, 10);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installion failed";
+
+    iret = sys.readMemory(10, 5, dummy_buffer);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Memory read failed";
 }
