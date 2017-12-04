39c48f0f4fd1bf1dab7c19f6acd409c8a9a6fe41
Andy Roxby <Andy.Roxby@gmail.com>
2017-12-04 02:52:17 -0500

    Write a bunch more tests

--- tests/test_system.cpp
+++ tests/test_system.cpp
@@ -9,8 +9,41 @@ protected:
     StrictMockMemoryModule mem;
 };
 
-//Dumy test
-TEST_F(SystemTest, TestMemoryAddressTranslation) {
+TEST_F(SystemTest, TestBasicMemoryInstall) {
+    int iret;
+
+    iret = sys.installMemory(mem, 10, 10);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installion failed";
+}
+
+TEST_F(SystemTest, TestZeroMemoryInstall) {
+    int iret;
+
+    iret = sys.installMemory(mem, 10, 0);
+    ASSERT_EQ(iret, ERR_BADRANGE) << "Invalid Module install succeeded";
+}
+
+TEST_F(SystemTest, TestOverlapMemoryInstall) {
+    int iret;
+
+    iret = sys.installMemory(mem, 5, 5);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installion failed";
+
+    iret = sys.installMemory(mem, 7, 5);
+    ASSERT_EQ(iret, ERR_CONFLICT) << "Invalid address install succeeded";
+}
+
+TEST_F(SystemTest, TestMultipleMemoryInstall) {
+    int iret;
+
+    iret = sys.installMemory(mem, 5, 5);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 1 installion failed";
+
+    iret = sys.installMemory(mem, 10, 5);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 2 installion failed";
+}
+
+TEST_F(SystemTest, TestBasicMemoryAddressTranslation) {
     int iret;
     unsigned char dummy_buffer[10];
 
@@ -21,5 +54,25 @@ TEST_F(SystemTest, TestMemoryAddressTranslation) {
     ASSERT_EQ(iret, ERR_SUCCESS) << "Module installion failed";
 
     iret = sys.readMemory(10, 5, dummy_buffer);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Memory read failed";
+}
+
+TEST_F(SystemTest, TestSpannedMemoryAddressTranslation) {
+    int iret;
+    unsigned char dummy_buffer[10];
+
+    EXPECT_CALL(mem, readMemory(5, 5, dummy_buffer))
+        .WillOnce(testing::Return(ERR_SUCCESS));
+
+    EXPECT_CALL(mem, readMemory(0, 5, dummy_buffer+5))
+        .WillOnce(testing::Return(ERR_SUCCESS));
+
+    iret = sys.installMemory(mem, 0, 10);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 1 installion failed";
+
+    iret = sys.installMemory(mem, 10, 5);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 2 installion failed";
+
+    iret = sys.readMemory(5, 10, dummy_buffer);
     ASSERT_EQ(iret, ERR_SUCCESS) << "Memory read failed";
 }
