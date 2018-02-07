bbc337ce01ce27948dd14c6576a907a8924adacc
Andy Roxby <aroxby@users.noreply.github.com>
2018-02-06 19:23:26 -0500

    Prepare System tests for generalization

--- tests/test_system.cpp
+++ tests/test_system.cpp
@@ -3,50 +3,49 @@
 #include <system.h>
 #include "mock_memorymodule.h"
 
-//#include <iostream>
-//using namespace std;
+#define TEST_CLASS SystemTest
 
-class SystemTest : public testing::Test {
+class TEST_CLASS : public testing::Test {
 protected:
     System sys;
     StrictMockMemoryModule mem;
 };
 
-TEST_F(SystemTest, TestBasicMemoryInstall) {
+TEST_F(TEST_CLASS, TestBasicMemoryInstall) {
     int iret;
 
     iret = sys.installMemory(mem, 10, 10);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installation failed";
 }
 
-TEST_F(SystemTest, TestZeroMemoryInstall) {
+TEST_F(TEST_CLASS, TestZeroMemoryInstall) {
     int iret;
 
     iret = sys.installMemory(mem, 10, 0);
     ASSERT_EQ(iret, ERR_BADRANGE) << "Invalid Module install succeeded";
 }
 
-TEST_F(SystemTest, TestOverlapMemoryInstall) {
+TEST_F(TEST_CLASS, TestOverlapMemoryInstall) {
     int iret;
 
     iret = sys.installMemory(mem, 5, 5);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installation failed";
 
     iret = sys.installMemory(mem, 7, 5);
     ASSERT_EQ(iret, ERR_CONFLICT) << "Invalid address install succeeded";
 }
 
-TEST_F(SystemTest, TestMultipleMemoryInstall) {
+TEST_F(TEST_CLASS, TestMultipleMemoryInstall) {
     int iret;
 
     iret = sys.installMemory(mem, 5, 5);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 1 installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 1 installation failed";
 
     iret = sys.installMemory(mem, 10, 5);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 2 installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 2 installation failed";
 }
 
-TEST_F(SystemTest, TestBasicMemoryAddressTranslation) {
+TEST_F(TEST_CLASS, TestBasicMemoryAddressTranslation) {
     int iret;
     unsigned char dummy_buffer[10];
 
@@ -54,13 +53,13 @@ TEST_F(SystemTest, TestBasicMemoryAddressTranslation) {
         .WillOnce(testing::Return(ERR_SUCCESS));
 
     iret = sys.installMemory(mem, 10, 10);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installation failed";
 
     iret = sys.readMemory(10, 5, dummy_buffer);
     ASSERT_EQ(iret, ERR_SUCCESS) << "Memory read failed";
 }
 
-TEST_F(SystemTest, TestSpannedMemoryAddressTranslation) {
+TEST_F(TEST_CLASS, TestSpannedMemoryAddressTranslation) {
     int iret;
     unsigned char dummy_buffer[10];
 
@@ -71,16 +70,16 @@ TEST_F(SystemTest, TestSpannedMemoryAddressTranslation) {
         .WillOnce(testing::Return(ERR_SUCCESS));
 
     iret = sys.installMemory(mem, 0, 10);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 1 installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 1 installation failed";
 
     iret = sys.installMemory(mem, 10, 5);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 2 installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 2 installation failed";
 
     iret = sys.readMemory(5, 10, dummy_buffer);
     ASSERT_EQ(iret, ERR_SUCCESS) << "Memory read failed";
 }
 
-TEST_F(SystemTest, TestNonContinuousMemoryAddressTranslation) {
+TEST_F(TEST_CLASS, TestNonContinuousMemoryAddressTranslation) {
     int iret;
     unsigned char dummy_buffer[10];
 
@@ -88,10 +87,10 @@ TEST_F(SystemTest, TestNonContinuousMemoryAddressTranslation) {
         .WillOnce(testing::Return(ERR_SUCCESS));
 
     iret = sys.installMemory(mem, 0, 10);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 1 installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 1 installation failed";
 
     iret = sys.installMemory(mem, 15, 5);
-    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 2 installion failed";
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module 2 installation failed";
 
     iret = sys.readMemory(5, 10, dummy_buffer);
     ASSERT_EQ(iret, ERR_BADRANGE) << "Bad memory read succeeded";
