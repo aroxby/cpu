5b2c9442f0b9fc024898588da515dd76616fd06a
Andy Roxby <aroxby@users.noreply.github.com>
2018-05-13 17:02:04 -0400

    Clean up design

--- src/jff/jff.cpp
+++ src/jff/jff.cpp
@@ -1,43 +1,51 @@
 #ifndef _INC_GENERICCPU_H
 #define _INC_GENERICCPU_H
 
+#include <queue>
 #include "basetypes.h"
 #include "basecpu.h"
 #include "instruction.h"
 
 class Interruptable {
 public:
+    virtual void signalInterrupt(SizeType interrupt);
+
+protected:
+    virtual bool serviceNextInterrupt();
     virtual void serviceInterrupt(SizeType interrupt) = 0;
-};
 
-class CPUConfig {
-public:
-    CPUConfig(
-        SizeType registerWidth, void *instructionPointer, SizeType maxInstuctionLength,
-        SizeType iBadInstruction, SizeType iDoubleFault, SizeType iTripleFault
-    ) : registerWidth(registerWidth), instructionPointer(instructionPointer), maxInstuctionLength(maxInstuctionLength),
-        iBadInstruction(iBadInstruction), iDoubleFault(iDoubleFault), iTripleFault(iTripleFault) {
-    }
-
-    SizeType registerWidth;
-    void *instructionPointer;
-    SizeType maxInstuctionLength;
-    SizeType iBadInstruction;
-    SizeType iDoubleFault;
-    SizeType iTripleFault;
+private:
+    typedef std::queue<SizeType> InterruptQueue;
+    InterruptQueue queue;
 };
 
 class GenericCPU : public BaseCPU, public Interruptable {
 public:
-    GenericCPU(const System &sys, const InstructionSet &set, CPUConfig &config): BaseCPU(sys), set(set), config(config) { }
+    GenericCPU(const System &sys, const InstructionSet &set, SizeType intBadInstruction) :
+        BaseCPU(sys), set(set) { }
     virtual void tick();
-    
-    virtual void reset() = 0;
-    virtual void serviceInterrupt(SizeType interrupt) = 0;
+
+    virtual SizeType registerWidth() = 0;
+    virtual void *instructionPointer() = 0;
+    virtual bool interruptsEnabled() = 0;
 
 private:
     const InstructionSet &set;
-    CPUConfig config;
+};
+
+class JFF : public GenericCPU {
+public:
+    JFF(const System &sys, const InstructionSet &set) : GenericCPU(sys, set, 1) { }
+
+    virtual void reset();
+    virtual void signalInterrupt(SizeType interrupt);
+
+    virtual SizeType registerWidth();
+    virtual void *instructionPointer();
+    virtual bool interruptsEnabled();
+
+protected:
+    virtual void serviceInterrupt(SizeType interrupt);
 };
 
 #endif//_INC_GENERICCPU_H
