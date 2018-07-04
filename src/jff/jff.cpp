1d31f7bbfd7bfaf6fe37d4868f236c8cdfc0ffd4
Andy Roxby <aroxby@users.noreply.github.com>
2018-07-04 12:49:29 -0400

    Clean up stub code

--- src/jff/jff.cpp
+++ src/jff/jff.cpp
@@ -3,5 +3,10 @@
 JFF::JFF(const System &sys, const InstructionSet &set) : GenericCPU(sys, set, 1, 2) {
 }
 
-void JFF::reset() { throw; }
-void JFF::serviceInterrupt(Interrupt interrupt) { throw; }
+void JFF::reset() {
+    throw "Not Implemented";
+}
+
+void JFF::serviceInterrupt(Interrupt interrupt) {
+    throw "Not Implemented";
+}
