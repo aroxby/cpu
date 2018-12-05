60b1ef2912a64b4e4e695e7afa3d3cdb650836ad
Andy Roxby <aroxby@users.noreply.github.com>
2018-12-05 15:29:54 -0500

    Create tests for Interruptable

--- tests/test_basecpu.cpp
+++ tests/test_basecpu.cpp
@@ -22,12 +22,24 @@ public:
     int maxTicks;
 };
 
+class TestInterruptable : public Interruptable {
+public:
+    virtual void serviceInterrupt(Interrupt interrupt) {
+        serviced.push_back(interrupt);
+    }
+
+    bool _next() { return serviceNextInterrupt(); }
+
+    std::vector<Interrupt> serviced;
+};
+
 const int TEST_TICK_COUNT = 3;
 
 class TEST_CLASS : public testing::Test {
 protected:
     TEST_CLASS() : cpu(TEST_TICK_COUNT, dummySystem) { }
     BaseTestCPU cpu;
+    TestInterruptable server;
     System dummySystem;
 };
 
@@ -39,4 +51,17 @@ TEST_F(TEST_CLASS, TestRunLoop) {
     ASSERT_FALSE(cpu.isRunning()) << "Incorrect final CPU running state";
     ASSERT_EQ(cpu.ticks, TEST_TICK_COUNT) << "Incorrect final CPU tick count";
     ASSERT_EQ(cpu.resets, 1) << "Incorrect initial CPU reset count";
+}
+
+TEST_F(TEST_CLASS, TestInterruptService) {
+    ASSERT_FALSE(server._next()) << "Incorrect service of empty queue";
+    server.signalInterrupt(3);
+    server.signalInterrupt(5);
+    ASSERT_TRUE(server._next()) << "Failed to service queue";
+    ASSERT_EQ(server.serviced.size(), 1) << "Incorrect number of interrupts serviced";
+    ASSERT_EQ(server.serviced.at(0), 3) << "Incorrect interrupt serviced";
+    ASSERT_TRUE(server._next()) << "Failed to service queue";
+    ASSERT_EQ(server.serviced.size(), 2) << "Incorrect number of interrupts serviced";
+    ASSERT_EQ(server.serviced.at(1), 5) << "Incorrect interrupt serviced";
+    ASSERT_FALSE(server._next()) << "Incorrect service of empty queue";
 }
