cb19514bf6587418e6a04fcafb3a7a9f5345b4ee
Andy Roxby <aroxby@users.noreply.github.com>
2018-12-29 19:59:06 -0500

    Add extra memory tests

--- tests/test_system.cpp
+++ tests/test_system.cpp
@@ -95,3 +95,40 @@ TEST_F(TEST_CLASS, TestNonContinuousMemoryAddressTranslation) {
     iret = sys.readMemory(5, 10, dummy_buffer);
     ASSERT_EQ(iret, ERR_BADRANGE) << "Bad memory read succeeded";
 }
+
+TEST_F(TEST_CLASS, TestMemoryRemove) {
+    int iret;
+
+    iret = sys.installMemory(mem, 10, 10);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installation failed";
+
+    iret = sys.installMemory(mem, 10, 10);
+    ASSERT_NE(iret, ERR_SUCCESS) << "Overlapping installation did not fail";
+
+    iret = sys.removeMemory(10);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module uninstallation failed";
+
+    iret = sys.installMemory(mem, 10, 10);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installation failed";
+}
+
+TEST_F(TEST_CLASS, TestMemoryReadWrite) {
+    const void *writeData = "123456789";  // 10 bytes with \0
+    unsigned char readData[10];
+    int iret;
+
+    EXPECT_CALL(mem, writeMemory(0, 10, writeData))
+        .WillOnce(testing::Return(ERR_SUCCESS));
+
+    EXPECT_CALL(mem, readMemory(0, 10, readData))
+        .WillOnce(testing::Return(ERR_SUCCESS));
+
+    iret = sys.installMemory(mem, 10, 10);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Module installation failed";
+
+    iret = sys.writeMemory(10, 10, writeData);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Memory read failed";
+
+    iret = sys.readMemory(10, 10, readData);
+    ASSERT_EQ(iret, ERR_SUCCESS) << "Memory read failed";
+}
